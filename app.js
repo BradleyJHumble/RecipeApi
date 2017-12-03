368af4854e9dee9cbfe8ef74c9b12483190d1757
judikial <bradleyjhumble@gmail.com>
2017-12-03 03:53:08 -0600

    reset

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
